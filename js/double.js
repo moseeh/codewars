56662ef8558d3b6caab9b2e6b5d6fedd3bae8776
moonyango <mosesadrian825@gmail.com>
2024-12-31 16:12:55 +0300

    fix: refactor

--- js/double.js
+++ js/double.js
@@ -1,8 +1,7 @@
 function doubleChar(str) {
     let newstr = ""
     for(let i = 0; i < str.length; i++) {
-        newstr += str[i]
-        newstr += str[i]
+        newstr += str[i].repeat(2)
     }
     return newstr
 }
